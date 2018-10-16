4a1bea981914cf133f0ca4a2ec47789ddce74f89
reedniv <26852596+reedniv@users.noreply.github.com>
2018-10-16 22:42:28 +0700

    Update safelinks.js

--- Javascript/safelinks.js
+++ Javascript/safelinks.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://encrypt-links.blogspot.co.id/p/redirect.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.encrypt-links.ga/p/redirect.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
